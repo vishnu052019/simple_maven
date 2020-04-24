96077eb51e014484b2224e8925e7aabe295166e8
vishnu052019 <47840811+vishnu052019@users.noreply.github.com>
2020-04-24 17:43:42 +0000

    project pipline

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world vishnucharan!!!!!!!!!!!!!!!!!!!!!!!!!!!welcome royi!";
+    return "Hello world vishnucharan!!!!!!!!!!!!!!!!!!!!!!!!!!!welcome royi joy!";
   }
 }
